71ae166197677992c932c695fee1722857e8dcf2
NLLLee <87617775+NLLLee@users.noreply.github.com>
2022-12-14 23:33:21 -0500

    Update Challenge1.py

--- Challenge1.py
+++ Challenge1.py
@@ -288,10 +288,6 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-# Testing code
-# print(f'Pssst, the solution is {chosen_word}.')
-
-# Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
@@ -299,20 +295,17 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
 
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
     # Check if user is wrong.
     if guess not in chosen_word:
-        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
 
         lives -= 1
@@ -320,10 +313,8 @@ while not end_of_game:
             end_of_game = True
             print("You lose.")
 
-    # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
-    # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
